de9ca66b24b5b7231cb720320c8008c7a120163b
Syed Furquan Ahmad <furquan@pm.me>
2022-05-29 17:43:45 +0530

    Fetch data from the json server using axios every 2 seconds, storing it in a useState

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
 const App = () => {
+  const appVersion = require('../package.json').version;
+  const URL = 'http://localhost:3001/issues';
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      axios
+        .get(URL)
+      .then(response => setData(response.data));
+    }, 2000);
+  }, [data]);
+
+  console.log(data);
+
   return (
-    <div className="App"></div>
+    <div className="App">
+      DevTrak v{appVersion}
+    </div>
   );
 };
 
