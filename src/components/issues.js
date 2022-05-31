1c4156719425018c8ec99e5e275f67b95f7ddafb
Syed Furquan Ahmad <furquan@pm.me>
2022-05-31 15:57:24 +0530

    Added Issues component to display the data in a TableContainer as a scrollable table

---
+++ src/components/issues.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import Checkbox from '@mui/material/Checkbox';
+
+const columns = [
+  { id: 'number', label: '#', minWidth: 100, align: 'left' },
+  { id: 'type', label: 'Type', minWidth: 100 },
+  {
+    id: 'project',
+    label: 'Project',
+    minWidth: 170,
+    align: 'center'
+  },
+  {
+    id: 'title',
+    label: 'Title',
+    minWidth: 170,
+    align: 'left'
+  }
+];
+
+const Issues = ({ rows }) =>
+  <TableContainer sx={{ maxHeight: 440 }}>
+    <Table stickyHeader aria-label="sticky table">
+      <TableHead>
+        <TableRow>
+          {columns.map((column) => (
+            <TableCell
+              key={column.id}
+              align={column.align}
+              style={{ minWidth: column.minWidth }}
+            >
+              {column.label}
+            </TableCell>
+          ))}
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {rows.map((row) => {
+          return (
+            <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+              {columns.map((column) => {
+                const value = row[column.id];
+                return (
+                  <TableCell key={column.id} align={column.align}>
+                    {column.format && typeof value === 'number'
+                      ? column.format(value)
+                      : value}
+                  </TableCell>
+                );
+              })}
+            </TableRow>
+          );
+        })}
+      </TableBody>
+    </Table>
+  </TableContainer>;
+
+export default Issues;
