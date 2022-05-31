794f5a1fdfc7588936d47fa6f43666b5c33b2b11
Syed Furquan Ahmad <furquan@pm.me>
2022-05-31 09:29:42 +0530

    Changed the color of IconButton to white using ThemeProvider

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import './header.css';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#ffffff'
+    },
+    secondary: {
+      main: '#f1faee'
+    }
+  }
+});
 
 const Header = ({ version }) =>
   <div className="header">
     <span id="header-title">DevTrak v{version}</span>
-    <IconButton aria-label="menu" size="large">
-      <MenuIcon />
-    </IconButton>
+    <ThemeProvider theme={theme}>
+      <IconButton aria-label="menu" size="large" color="primary">
+        <MenuIcon />
+      </IconButton>
+    </ThemeProvider>
   </div>;
 
 export default Header;
