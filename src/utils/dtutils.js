d2f5b6403283b22044f6d0962b4c665ac19ab8cc
Syed Furquan Ahmad <furquan@pm.me>
2022-05-31 17:05:29 +0530

    Utility functions for DevTrak

---
+++ src/utils/dtutils.js
@@ -0,0 +1,18 @@
+import { createTheme } from '@mui/material/styles';
+
+const createData = (closed, number, type, project, title) => {
+  return { closed, number, type, project, title };
+}
+
+const getPriorityColor = (priority) => {
+  switch (priority) {
+    case 0: return '#ADD8E6'; // lightblue
+    case 1: return '#FF0000'; // red
+    case 2: return '#FFA500'; // orange
+    case 3: return '#FFFF00'; // yellow
+    case 4: return '#008000'; // green
+    default: return '#6495ED'; // gray
+  }
+}
+
+export default { createData, getPriorityColor };
