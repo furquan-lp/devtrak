1f6daf4c2e5f135a65b954e549509f370d7cc7e9
Syed Furquan Ahmad <furquan@pm.me>
2022-05-31 09:14:03 +0530

    Added menu IconButton to Header

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
 import './header.css';
 
 const Header = ({ version }) =>
   <div className="header">
     <span id="header-title">DevTrak v{version}</span>
+    <IconButton aria-label="menu" size="large">
+      <MenuIcon />
+    </IconButton>
   </div>;
 
 export default Header;
