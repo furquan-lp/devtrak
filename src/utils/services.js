36bc38982945b74c9bc1fc0832426ae5b2c49ec0
Syed Furquan Ahmad <furquan@pm.me>
2022-06-02 21:50:40 +0530

    Removed changeProject and added changeData in its place, and use getURL and postURL for fetching and modification respectively

--- src/utils/services.js
+++ src/utils/services.js
@@ -1,17 +1,18 @@
 import axios from 'axios';
 
-const dataURL = 'http://localhost:3001/projects';
+const getURL = 'https://api.jsonbin.io/v3/b/6298bb7f449a1f3821fb3659/latest';
+const postURL = 'https://api.jsonbin.io/v3/b/6298bb7f449a1f3821fb3659';
 
 const getProjects = (requestHeader) => {
   if (requestHeader === undefined)
-    return axios.get(dataURL).then(response => response.data);
+    return axios.get(getURL).then(response => response.data);
   else
     return axios
-      .get(dataURL, { headers: requestHeader })
+      .get(getURL, { headers: requestHeader })
       .then(response => response.data);
 }
 
-const changeProject = (project, id) =>
-  axios.put(`http://localhost:3001/projects/${id}`, project);
+const changeData = (data, requestHeader) =>
+  axios.put(postURL, data, { headers: requestHeader });
 
-export default { getProjects, changeProject };
+export default { getProjects, changeData };
