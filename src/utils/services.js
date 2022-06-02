f6dcb0eda349dbf427cb6746440377719c3e2db8
Syed Furquan Ahmad <furquan@pm.me>
2022-06-02 18:56:36 +0530

    Added the option to pass request headers to getProjects

--- src/utils/services.js
+++ src/utils/services.js
@@ -2,8 +2,14 @@ import axios from 'axios';
 
 const dataURL = 'http://localhost:3001/projects';
 
-const getProjects = () =>
-  axios.get(dataURL).then(response => response.data);
+const getProjects = (requestHeader) => {
+  if (requestHeader === undefined)
+    return axios.get(dataURL).then(response => response.data);
+  else
+    return axios
+      .get(dataURL, { headers: requestHeader })
+      .then(response => response.data);
+}
 
 const changeProject = (project, id) =>
   axios.put(`http://localhost:3001/projects/${id}`, project);
