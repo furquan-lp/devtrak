635d37096c34a9368e69561aa6378ef609195458
Syed F Ahmad <furquan@protonmail.com>
2023-01-20 18:27:02 +0530

    Added the animated background containers to root

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <div class="bg"></div>
+    <div class="bg bg2"></div>
+    <div class="bg bg3"></div>
     <App />
   </React.StrictMode>
 );
